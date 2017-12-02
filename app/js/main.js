1db645d5ca38ddb7a586168027c691c5e384830b
Jaydeep Patel <jaydeep.patel@outware.com.au>
2017-12-02 21:34:45 +1100

    added gulp support

---
+++ app/js/main.js
@@ -0,0 +1,45 @@
+(function () {
+
+  'use strict';
+
+  require('angular');
+  require('angular-route');
+  require('angular-resource');
+  require('angular-sanitize');
+
+  var busApp = angular.module('busApp', ['ngRoute', 'ngResource', 'ngSanitize']);
+
+  busApp.constant('apiUrl', 'http://localhost:3000/data');
+
+  busApp.filter('formatVariant', function() {
+    return function (input) {
+      return '<strong>' + input.substr(0, 3) + '</strong>' + input.substr(3);
+    }
+  });
+
+  busApp.factory('Buses', ['$resource', 'apiUrl', function($resource, apiUrl) {
+    return $resource(apiUrl);
+  }]);
+
+  busApp.config(function($routeProvider) {
+    var resolveBuses = {
+      buses: function(Buses) {
+        return Buses.query().$promise;
+      }
+    };
+
+    $routeProvider
+      .when('/', {
+        controller:'BusesController as busesCtrl',
+        templateUrl:'./partials/buseslist.html',
+        resolve: resolveBuses
+      })
+      .otherwise({ redirectTo: '/' });
+  });
+
+  busApp.controller('BusesController', function(buses, $location) {
+    var busesCtrl = this;
+    busesCtrl.buses = buses;
+  });
+
+}());
